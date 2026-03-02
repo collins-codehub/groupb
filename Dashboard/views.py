21de564e0d2bedccf62586cbcba53fa12f4e9765
collins-codehub <collins100percent@gmail.com>
2026-03-02 13:47:15 +0300

    second commit

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,9 +1,13 @@
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
-
 
 from Dashboard.models import Student
 
 # Create your views here.
+@login_required
 def dashboard(request):
     students = Student.objects.all()
     return render(request, 'dashboard.html', context={'students': students})
@@ -42,3 +46,29 @@ def update_student(request, id):
         student.save()
         return redirect('dashboard')
     return render(request, 'update_student.html', {'student': student})
+# delete
+def delete_student(request, id):
+    student = get_object_or_404(Student, id=id)
+
+    if request.method == 'POST':
+        student.delete()
+        return redirect('dashboard')
+
+
+    return render('dashboard')
+
+# *new sign up
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        if User.objects.filter(username=username).exists():
+            messages.info(request, 'Username already exists')
+            return redirect('signup')
+        user = User.objects.create_user(username=username, email=email, password=password)
+
+        login(request, user)
+        return redirect('dashboard')
+    return render(request, 'sign_up.html')
